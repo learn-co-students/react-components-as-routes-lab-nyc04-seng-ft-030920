1641ef971d56a4c31d6d920b961fbf9734ce64b5
Xavier Carty <xaviercarty@gmail.com>
2020-05-21 13:36:04 -0400

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,9 +12,17 @@ import Movies from '../components/Movies';
 
 const App = (props) => {
   return (
+    <div>
     <Router>
-      {/*{code here}*/}
+      <div>
+      <NavBar/>
+      <Route exact path="/" component={Home} />
+      <Route exact path = "/actors" component={Actors}/>
+      <Route exact path = "/movies" component={Movies}/>
+      <Route exact path = "/directors" component={Directors}/>
+      </div>
     </Router>
+    </div>
   );
 };
 
